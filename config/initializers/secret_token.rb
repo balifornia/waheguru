9907fd2b2fc5d1b428ee885979814b17db224057
Tarandeep Bali <tarandeep_bali@yahoo.com>
2013-03-07 17:43:07 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waheguru::Application.config.secret_token = '98ab121af037fd49212290aef2ba114e4f1215fe0471ab79a995e97d81f201f8b9b5fa28bccc9bad536ec1d7e9874eeaa3b86e7b5f1c2ab90e1e1253204428c7'
